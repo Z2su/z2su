7c11e8536b0cf7df32ef37bf63f46571a09f91ec
mimi <mimi@gmail.com>
2019-05-23 18:10:48 +0900

    springMVC

---
+++ springMVC_test/src/main/java/com/spring/controller/CommonController.java
@@ -0,0 +1,46 @@
+package com.spring.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.dto.MemberVO;
+import com.spring.service.MemberService;
+
+@Controller
+public class CommonController {
+	@Resource(name="memberService")
+	private MemberService memberService;
+
+	@RequestMapping(value="/common/login", method=RequestMethod.GET)
+	public String loginGet() {
+		return "common/login";
+	}
+	@RequestMapping(value="/common/login", method=RequestMethod.POST)
+	public void loginPOST(LoginRequest loginReq, HttpServletResponse response) throws SQLException, IOException {
+		MemberVO member = memberService.getMember(loginReq.getId());
+		String message ="";
+		if(member!=null) {
+			if(loginReq.getPwd().equals(member.getPwd())) {
+				
+				message="로그인에 성공했습니다.";
+			}else {
+				message="패스워드가 일치하지 않습니다.";
+				
+			}
+		}else {
+			message="존재하지않는 아이디입니다.";
+		}
+		
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<script>alert('"+message+"');</script>");
+	}
+}
